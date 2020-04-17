aedd16368bdf57a9b432970f0b2df46fe0b99faf
unknown <kha.led002@hotmail.com>
2020-04-17 15:15:43 -0300

    first commit

---
+++ src/repositories/TransactionsRepository.ts
@@ -0,0 +1,55 @@
+import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    return this.transactions;
+  }
+
+  public getBalance(): Balance {
+    const balance = this.transactions.reduce(
+      (previous: Balance, current: Transaction) => {
+        if (current.type === 'income') {
+          previous.income += current.value;
+        } else {
+          previous.outcome += current.value;
+        }
+        previous.total = previous.income - previous.outcome;
+        return previous;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    return balance;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
